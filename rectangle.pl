de81898bbf6b9f2185ee48bcb8a02ed127f593f9
Nivedhitha D <50760049+nive927@users.noreply.github.com>
2020-09-22 20:03:30 +0530

    Add files via upload

---
+++ rectangle.pl
@@ -0,0 +1,10 @@
+point(1,1).
+point(1,2).
+point(1,3).
+point(2,1).
+point(2,2).
+point(2,3).
+point(3,1).
+point(3,2).
+point(3,3).
+rectangle(point(X1, Y1), point(X1, Y2), point(X2, Y1), point(X2, Y2)).
